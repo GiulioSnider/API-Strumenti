1c7016e236e07b67b25c6c2934f7f5f96e955da6
GiulioSnider <giuliosnider@gmail.com>
2022-10-14 15:31:24 +0200

    Add e Get INstruments da FILE

---
+++ API Strumenti/Controllers/MusicInstrumentController.cs
@@ -0,0 +1,23 @@
+﻿using API_Strumenti.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Strumenti.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicInstrumentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllInstruments()
+        {
+            var instrumentList = FileHelper.GetMusicInstruments();
+            return Ok(instrumentList);
+        }
+
+        [HttpPost]
+        public IActionResult AddInstrument([FromBody] MusicInstrument musicInstrument)
+        {            
+            return Ok(FileHelper.AddInstrument(musicInstrument));
+        }
+    }
+}
---
+++ API Strumenti/FileHelper.cs
@@ -0,0 +1,37 @@
+﻿using API_Strumenti.Model;
+using System.Text.Json;
+
+namespace API_Strumenti
+{
+    public static class FileHelper
+    {
+        private const string _path = "C:\\Users\\user\\Documents\\ACADEMY UNIKEY\\02.BACKEND" +
+            "\\Esercizi\\MusicInstrumentsDB\\StrumentiMusicali.txt";
+        public static MusicInstrument AddInstrument(MusicInstrument musicInstrument)
+        {
+            var fileContent = File.ReadAllText(_path);
+            var musicInstrumentsDeserialize = JsonSerializer.Deserialize<List<MusicInstrument>>(fileContent);
+            if (musicInstrumentsDeserialize != null)
+            {
+                musicInstrumentsDeserialize.Add(musicInstrument);
+            }
+            else
+            {
+                throw new Exception("File vuoto!");
+            }
+            var SerializedList = JsonSerializer.Serialize(musicInstrumentsDeserialize);
+            File.WriteAllText(_path, SerializedList);
+            return musicInstrument;
+        }
+        public static IEnumerable<MusicInstrument> GetMusicInstruments()
+        {            
+            var musicInstrumentsDeserialize = JsonSerializer.Deserialize<List<MusicInstrument>>(File.ReadAllText(_path));
+            if (musicInstrumentsDeserialize == null)
+            {
+                throw new Exception("Niente strumenti");
+            }
+            return musicInstrumentsDeserialize;
+        }
+        
+    }
+}
