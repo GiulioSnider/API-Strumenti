57b1e770874e26e388aa036bde23776b5184777d
GiulioSnider <giuliosnider@gmail.com>
2022-10-14 14:34:59 +0200

    Add project files.

---
+++ API Strumenti/Model/MusicInstrument.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Strumenti.Model
+{
+    public class MusicInstrument
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
